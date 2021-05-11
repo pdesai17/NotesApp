b8485d8b8c0186af5e3efb9b321c8450b3e12c29
PRARTHANA <prarthana.desai17@gmail.com>
2021-05-11 19:31:46 +0530

    create notes

--- app/src/main/java/com/example/notesapp/NotesActivity.java
+++ app/src/main/java/com/example/notesapp/NotesActivity.java
@@ -1,14 +1,52 @@
 package com.example.notesapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.notesapp.databinding.ActivityNotesBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NotesActivity extends AppCompatActivity {
+    ActivityNotesBinding binding;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+        binding = ActivityNotesBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        binding.addNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NotesActivity.this, CreateNotesActivity.class));
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.log_out,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.logOut:
+                firebaseAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
